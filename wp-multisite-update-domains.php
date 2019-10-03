eaa16fd97d455d8ad876ddfa39bea4a1a0c71f83
Jonas Kroeger <jk@devicezero.me>
2019-10-03 15:22:29 +0200

    test

--- wp-multisite-update-domains.php
+++ wp-multisite-update-domains.php
@@ -8,9 +8,9 @@ require __DIR__.'/vendor/autoload.php';
 // You can alternatively use getenv() yourself.
 $config = new Config();
 
-if (!$config->isValidPlatform()) {
-    die("Not in a Platform.sh Environment.");
-}
+// if (!$config->isValidPlatform()) {
+//     die("Not in a Platform.sh Environment.");
+// }
 
 # get primary domain
 $primaryRouteArray = array_filter($config->routes(), function($k) {
@@ -26,3 +26,6 @@ $blogs = $blogsQuery->fetch_all();
 print_r($blogs);
 
 // "UPDATE wp_blogs SET domain = '{$blogDomain}' WHERE blog_id = {$blogId};
+foreach ($blogs as $blog) {
+	echo "UPDATE wp_blogs SET domain = '{$blog[1]}.{$primaryDomain}' WHERE blog_id = {$blog[0]};
+}
